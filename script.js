620b6c43c45959b0ade895a3d50867a97608f51f
aridarsan <aridarsan09@gmail.com>
2020-11-03 23:31:58 +0700

    update js & ui

--- script.js
+++ script.js
@@ -1,147 +1,159 @@
 // promise chain
-const getUserPromsChain = (id) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-      if (!id) {
-      reject(Error("Invalid ID"));
-      }
+const getUserPromsChain = () => {
+  let id = document.getElementById("input-chainProms").value;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (!id && typeof id !== number) {
+    reject(Error("Invalid ID"));
+    }
+
+    let response;
+    if (id > 10 ) {
+      response = {
+      success: false,
+      id: id,
+      message: "User Not Found!",
+      };
+    } else {
+      response = {
+      success: true,
+      id: id,
+      message: "User Found",
+      };
+    }
+    resolve(response);
+    }, 2000);
+  });
+};
   
-      let response;
-      if (id > 10) {
-        response = {
-        success: false,
-        id: id,
-        message: "User Not Found!",
-        };
-      } else {
-        response = {
-        success: true,
-        id: id,
-        message: "User Found",
-        };
-      }
-      resolve(response);
-      }, 2000);
+const handlePromsChain = () => {
+  const result = document.querySelector("#chainProms");
+  result.innerText = "Loading..."
+  console.log("Clicked...");
+  getUserPromsChain()
+    .then((result) => {
+      console.log(result);
+      document.querySelector("#chainProms").innerText = "ID " + result.id + " " + result.message
+    })
+    .catch((err) => {
+      console.log(err);
+      result.innerText = err
     });
-  };
-  
-  const handlePromsChain = () => {
-    console.log("Clicked...");
-    getUserPromsChain(10)
-      .then((result) => {
-        const dataUser = result;
-        console.log(dataUser);
-        document.querySelector("#chainProms").innerText = result.id + " " +result.message
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+};
 
 //   async await
-const getUserAsyncAwait = (id) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-      if (!id) {
-      reject(Error("Invalid ID"));
-      }
-  
-      let response;
-      if (id > 10) {
-        response = {
-        success: false,
-        id: id,
-        message: "User Not Found!",
-        };
-      } else {
-        response = {
-        success: true,
-        id: id,
-        message: "User Found",
-        };
-      }
-      resolve(response);
-      }, 2000);
-    });
-  };
-  
-  const handleAsyncAwait = async () => {
-    console.log("Clicked...");
-    try {
-    const dataUser = await getUserAsyncAwait(12);
-    console.log(dataUser);
-    document.querySelector("#asyncAwait").innerText = dataUser.id + " " +dataUser.message
-    } catch (err) {
-    console.log(err);
+const getUserAsyncAwait = () => {
+  let id = document.getElementById("input-asyncAwait").value;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (!id) {
+    reject(Error("Invalid ID"));
     }
-  };
+
+    let response;
+    if (id > 10) {
+      response = {
+      success: false,
+      id: id,
+      message: "User Not Found!",
+      };
+    } else {
+      response = {
+      success: true,
+      id: id,
+      message: "User Found",
+      };
+    }
+    resolve(response);
+    }, 2000);
+  });
+};
+  
+const handleAsyncAwait = async () => {
+  const result = document.querySelector("#asyncAwait");
+  result.innerText = "Loading..."
+  console.log("Clicked...");
+  try {
+  const dataUser = await getUserAsyncAwait();
+  console.log(dataUser);
+  document.querySelector("#asyncAwait").innerText = "ID " + dataUser.id + " " + dataUser.message
+  } catch (err) {
+  console.log(err);
+  }
+};
 
 //   promise all
-const getUserPromsAll = (id) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-      if (!id) {
-      reject(Error("Invalid ID"));
-      }
+const getUserPromsAll = () => {
+  let id = document.getElementById("input-promiseAll").value;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (!id) {
+    reject(Error("Invalid ID"));
+    }
+
+    let response;
+    if (id > 10) {
+      response = {
+      success: false,
+      id: id,
+      message: "User Not Found!",
+      };
+    } else {
+      response = {
+      success: true,
+      id: id,
+      message: "User Found",
+      };
+    }
+    resolve(response);
+    }, 2000);
+  });
+};
   
-      let response;
-      if (id > 10) {
-        response = {
-        success: false,
-        id: id,
-        message: "User Not Found!",
-        };
-      } else {
-        response = {
-        success: true,
-        id: id,
-        message: "User Found",
-        };
-      }
-      resolve(response);
-      }, 2000);
+Promise.all
+const handlePromsAll = () => {
+  const result = document.querySelector("#promiseAll");
+  result.innerText = "Loading..."
+  console.log("Clicked...");
+  Promise.all([getUserPromsAll(11)])
+    .then(([response]) => {
+      const dataUser = response;
+      console.log(dataUser);
+      document.querySelector("#promiseAll").innerText = "ID " + dataUser.id + " " + dataUser.message
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  };
-  
-  Promise.all
-  const handlePromsAll = () => {
-    console.log("Clicked...");
-    Promise.all([getUserPromsAll(11)])
-      .then((response) => {
-        const dataUser = response[0];
-        console.log(dataUser);
-        document.querySelector("#promiseAll").innerText = dataUser.id + dataUser.message
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+};
   
   //Promise all from module
 
 const getFirstName = () => {
-    const promise  = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        ////ternary operation (short hand if else)
-        return (true) ? resolve("Binar") : reject(Error("Gagal"))
-      }, 3000);
-    });
-    return promise;
-  };
-  
-  const getLastName = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        //ternary operation (short hand if else)
-        return (true) ? resolve(` Academy`) : reject(Error("Gagal")) 
-      }, 3000)
-    });
-  };
-  
-  const handleOnClick = () => {
-    console.log("Tunggu sebentar...")
-    Promise.all([getFirstName(), getLastName()])
-    .then(([hasil1, hasil2]) =>{
-      document.querySelector("#promiseAllModule").innerText = hasil1 + hasil2
-    })
-  }
+  let id = document.getElementById("input-promiseAllModule").value;
+  const promise  = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      ////ternary operation (short hand if else)
+      return (true) ? resolve("Binar") : reject(Error("Gagal"))
+    }, 3000);
+  });
+  return promise;
+};
+
+const getLastName = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //ternary operation (short hand if else)
+      return (true) ? resolve(` Academy`) : reject(Error("Gagal")) 
+    }, 3000)
+  });
+};
+
+const handleOnClick = () => {
+  const result = document.querySelector("#promiseAllModule");
+  result.innerText = "Loading..."
+  console.log("Tunggu sebentar...")
+  Promise.all([getFirstName(), getLastName()])
+  .then(([hasil1, hasil2]) =>{
+    document.querySelector("#promiseAllModule").innerText = hasil1 + hasil2
+  })
+}
